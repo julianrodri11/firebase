cc39bced1f73a10f344235959d67cac65d158799
Julian Rodriguez <julianrodri11@gmail.com>
2018-08-08 02:54:31 -0500

    Ingresar con firebase y javascript

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 function registrar()
-{
+{   //se reciben las variables de las cajas de texto
     var email = document.getElementById('email').value;
     var contrasena = document.getElementById('contrasena').value;
-
+    
+    //imprimo variables para ver si llegan bien
     // alert('email: '+email+'contraseña: '+contrasena);
     // console.log(email);
     // console.log(contrasena);
@@ -18,4 +19,21 @@ function registrar()
 
       });
 
+}
+
+function ingresar()
+{   //se reciben las variables de las cajas de texto
+    var email2 = document.getElementById('email2').value;
+    var contrasena2 = document.getElementById('contrasena2').value;
+    //imprimo variables para ver si llegan bien
+    console.log(email2);
+    console.log(contrasena2);
+
+    firebase.auth().signInWithEmailAndPassword(email2, contrasena2)
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
 }
