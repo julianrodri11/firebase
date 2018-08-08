53bcde14cba4240f9ab229202819e94d501c3cac
Julian Rodriguez <julianrodri11@gmail.com>
2018-08-08 04:46:51 -0500

    Enviar confirmaciòn de registro de correo

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ function registrar() { //se reciben las variables de las cajas de texto
     // console.log(email);
     // console.log(contrasena);
     firebase.auth().createUserWithEmailAndPassword(email, contrasena)
+        .then(function () {
+            verificarCorreo();
+        })
         .catch(function (error) {
             // Handle Errors here.
             var errorCode = error.code;
@@ -28,6 +31,10 @@ function ingresar() { //se reciben las variables de las cajas de texto
     console.log(contrasena2);
 
     firebase.auth().signInWithEmailAndPassword(email2, contrasena2)
+    .then(function()
+    {
+        aparece();    
+    })
         .catch(function (error) {
             // Handle Errors here.
             // var errorCode = error.code;
@@ -41,11 +48,14 @@ function observadordeestado() {
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
             console.log('Existe usuario activo');
-            aparece();
+            
             // User is signed in.
             var displayName = user.displayName;
             var email = user.email;
             console.log(user);
+            console.log('*******************');
+            console.log(user.emailVerified);
+            console.log('*******************');
             var emailVerified = user.emailVerified;
             var photoURL = user.photoURL;
             var isAnonymous = user.isAnonymous;
@@ -61,37 +71,45 @@ function observadordeestado() {
 }
 
 // inicio el observadordeestado() 
-observadordeestado() ;
+observadordeestado();
 
 //función que sirve para mostrar un mensaje solo cuando el usuario este activo, es llamada 
 //en el observador de estado
-function aparece()
-{
-    var contenido= document.getElementById('contenido');
-    contenido.innerHTML='<h1>Bienvenido</h1><br>  <button type="button" class="btn btn-primary" onclick="cerrarSesion()">Cerrar Sesión</button>';
-    contenido.style.display="block";
-    
+function aparece() {
+    var contenido = document.getElementById('contenido');
+    contenido.innerHTML = '<h1>Bienvenido</h1><br>  <button type="button" class="btn btn-primary" onclick="cerrarSesion()">Cerrar Sesión</button>';
+    contenido.style.display = "block";
+
 }
 // función que sirve para quitar el contenido del div de mensajes y ocultarlo
-function desaparece()
-{
-    var contenido= document.getElementById('contenido');
-    contenido.innerHTML='';
-    contenido.style.display="none";
+function desaparece() {
+    var contenido = document.getElementById('contenido');
+    contenido.innerHTML = '';
+    contenido.style.display = "none";
 }
 
 // función que sirve para cerrar la sesión de un usuario activo
-function cerrarSesion()
-{
+function cerrarSesion() {
     firebase.auth().signOut()
-    .then(
-        function(){
-            console.log('Saliendo...');
-            alert('Sesión Cerrada');
-            desaparece();
+        .then(
+            function () {
+                console.log('Saliendo...');
+                alert('Sesión Cerrada Exitosamente');
+                desaparece();
+            })
+        .catch(function (error) {
+            console.log(error);
         })
-    .catch(function(error)
-    {
-        console.log(error);
-    })
+}
+
+// función que sirve enviar un correo a un usuario que se registre por primera vez
+function verificarCorreo() {
+    var user = firebase.auth().currentUser;
+    user.sendEmailVerification().then(function () {
+        // Email sent.
+        console.log('Se ha enviado un correo de notificación');
+    }).catch(function (error) {
+        // An error happened.
+        console.log('Error, no se ha podido enviar un correo de notificación');
+    });
 }
